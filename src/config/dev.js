3d6073138491cd7f4b0b2bec714aa7ce40464400
Hameed Damee <dameeimpact@gmail.com>
2020-11-01 03:41:25 +0100

    dev pruned

--- src/config/dev.js
+++ src/config/dev.js
@@ -11,14 +11,10 @@ module.exports = {
   dbURI: "mongodb://localhost:27017/schedool",
   jwtSecret: "3p48-94i1u08qfhdj489135u0t9324i=2r02jf449u130",
   tokenType: "Bearer",
-  redisUrl: "redis://127.0.0.1:6379",
   logs: {
     level: process.env.LOG_LEVEL || "silly",
   },
   api: {
     prefix: process.env.API_PREFIX,
-  },
-  emails: {
-    apiKey: process.env.SENDGRID_API_KEY,
   },
 };
